4bf855ce2d1d95613b3190375bb4620b493816c2
Luthfia39 <luthfia.nisa2703@mail.ugm.ac.id>
2025-05-07 12:27:22 +0700

    fix: sorting number in sidebar

--- app/Filament/Resources/TemplateResource.php
+++ app/Filament/Resources/TemplateResource.php
@@ -24,6 +24,10 @@ class TemplateResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?int $navigationSort = 2;
+
+    protected static ?string $navigationLabel = 'Surat Keluar';
+
     public static function form(Form $form): Form
     {
         return $form
