094e1250d823b4a07f854540df145ef15d494836
Luthfia39 <luthfia.nisa2703@mail.ugm.ac.id>
2025-06-06 14:39:38 +0700

    fix: modify button create

--- app/Filament/Resources/TemplateResource/Pages/CreateSuratFromTemplate.php
+++ app/Filament/Resources/TemplateResource/Pages/CreateSuratFromTemplate.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\TemplateResource\Pages;
 use App\Filament\Resources\TemplateResource;
 use Filament\Resources\Pages\CreateRecord;
 use Filament\Notifications\Notification;
+use Filament\Actions\Action;
 
 class CreateSuratFromTemplate extends CreateRecord
 {
@@ -18,5 +19,28 @@ class CreateSuratFromTemplate extends CreateRecord
             ->body('Template surat baru telah berhasil ditambahkan.')
             ->success()
             ->send();
+    }
+
+    /**
+     * Mengatur aksi tombol di bagian bawah form.
+     * Kita akan filter untuk menghilangkan 'createAndCreateAnotherAction'.
+     */
+    protected function getFormActions(): array
+    {
+        // Panggil metode parent untuk mendapatkan aksi default
+        $actions = parent::getFormActions();
+
+        // Filter aksi untuk menghilangkan 'createAndCreateAnotherAction'
+        return array_filter($actions, fn (Action $action) => $action->getName() !== 'createAnother');
+    }
+
+    /**
+     * Mengatur URL redirect setelah record berhasil dibuat.
+     * Diarahkan ke halaman daftar (index) dari resource.
+     */
+    protected function getRedirectUrl(): string
+    {
+        // Mengarahkan ke halaman daftar (index) dari resource saat ini
+        return $this->getResource()::getUrl('index');
     }
 }
