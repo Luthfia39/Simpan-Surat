c497d3b4d51dabf5db24bf5bafd25e61b7b1ad08
Luthfia39 <luthfia.nisa2703@mail.ugm.ac.id>
2025-07-23 11:51:55 +0700

    fix: routing

--- resources/views/filament/pages/profile.blade.php
+++ resources/views/filament/pages/profile.blade.php
@@ -23,7 +23,7 @@
     @script
         <script>
             Livewire.on('reload-page', () => {
-                window.location.href = '/profile';
+                window.location.href = '/user/profile';
             });
         </script>
     @endscript
